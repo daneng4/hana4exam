96fc07885fe12c6b999f14225c30cae6c664b5cb
daneng4 <daneng4@naver.com>
2024-09-11 12:57:01 +0900

    update ex3

--- FEBasic/김은서/ex3.js
+++ FEBasic/김은서/ex3.js
@@ -1,3 +1,18 @@
 Array.prototype.sortBy = function (sortProp = '') {
-  return this;
+
+  const sortOrders = sortProp.split(',').map(sortOrder => {
+    const [prop, condition = 'asc'] = sortOrder.split(':');
+    return {prop, condition : condition.toLowerCase() === 'desc' ? -1 : 1};
+  });
+
+  return this.sort((u1, u2) => {
+    for(const order of sortOrders){
+      const {prop, condition} = order;
+      if(u1[prop] < u2[prop])
+        return -1 * condition;
+      if(u1[prop] > u2[prop])
+        return 1 * condition;
+    }
+    return 0;
+  });
 };
