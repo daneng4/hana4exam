f534326efde77414b60ac0c74481bd1e056de7e5
daneng4 <daneng4@naver.com>
2024-09-11 17:11:13 +0900

    update ex4

--- FEBasic/김은서/ex4.js
+++ FEBasic/김은서/ex4.js
@@ -1,3 +1,32 @@
-function deepCopy(obj) {}
+function deepCopy(obj) {
+    if(obj === null || typeof obj !== 'object')
+        return obj;
+    
+    const newObj = {};
 
+    if(Array.isArray(obj)){
+        return obj.map(element => deepCopy(element));
+    }else if(obj instanceof Map){
+        return new Map(obj.entries());
+    }else if(obj instanceof WeakMap){
+        return obj;
+    }else if(obj instanceof Set){
+        return new Set([...obj].map(value => deepCopy(value)));
+    }else if(obj instanceof WeakSet){
+        return obj;
+    }else if(Object.getOwnPropertySymbols(obj) === Symbol){
+        return Reflect.ownKeys(obj);
+    }else{
+        for(const k in obj){    
+            newObj[k] = deepCopy(obj[k]);
+        }
+    }
+    
+    const keys = Reflect.ownKeys(obj);
+    for(const key of keys){
+        newObj[key] = deepCopy(obj[key]);
+    }
+
+    return newObj;
+}
 module.exports = { deepCopy };
