ddef8ea4d1fe4f9c0902b48d2f1bc94f555d1dce
daneng4 <daneng4@naver.com>
2024-09-11 23:56:41 +0900

    update ex6

--- FEBasic/김은서/ex6.ts
+++ FEBasic/김은서/ex6.ts
@@ -1,4 +1,28 @@
 export const randTime = <T>(val: T): Promise<T> =>
   new Promise(resolve => setTimeout(resolve, Math.random() * 1000, val));
 
-export function promiseAllSettled<T>(promises: Promise<T>[]) {}
+type promiseResult<T> = | { status: "fulfilled"; value: T }
+| { status: "rejected"; reason: any };
+
+export function promiseAllSettled<T>(promises: Promise<T>[]): Promise<promiseResult<T>[]> {
+  
+  return new Promise((resolve) => {
+    const results: PromiseSettledResult<T>[] = [];
+    let count = 0;
+
+    promises.forEach((promise, index) => {
+      promise.then((val) => {
+        results[index] = {status: "fulfilled", value: val};
+      })
+      .catch((error) => {
+        results[index] = {status: "rejected", reason: error};
+      })
+      .finally(() => {
+        count+=1;
+        if(count === promises.length){
+          resolve(results);
+        }
+      });
+    });
+  });
+}
